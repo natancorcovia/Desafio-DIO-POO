724c13e9b57f9f19443f472e9bb85128680a5e17
Natan Corcovia <natankorkov@gmail.com>
2025-08-07 00:46:58 -0300

    feat : defining methods of the Dev class

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,6 +1,7 @@
 package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -8,11 +9,27 @@ public class Dev {
     private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp){};
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudoInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevInscritos().add(this);
+    };
 
-    public void progredir(){};
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudoInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudoInscritos.remove(conteudo.get());
+        } else{
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    };
 
-    public void calcularXp(){}
+    public double calcularTotalXp(){
+        return this.conteudoConcluidos
+        .stream()
+        .mapToDouble(Conteudo :: calcularXp)
+        .sum();
+    }
 
     public String getNome() {
         return nome;
