4c1e8f55de9f16dbfb4ffb7e7cfd24da6df10892
Natan Corcovia <natankorkov@gmail.com>
2025-08-07 00:01:20 -0300

    feat : create the Conteudo class

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,40 +2,28 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
     private LocalDate data;
 
+     @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
     public Mentoria() {
-    }
-
-    public String getTitulo(){
-        return titulo;
-    }
-
-    public String getDescricao(){
-        return descricao;
     }
 
     public LocalDate getData(){
         return data;
     }
 
-    public void setTitulo(String novoTitulo){
-        titulo = novoTitulo;
-    }
-
-    public void setDescricao(String novaDescricao){
-        descricao = novaDescricao;
-    }
      public void setData(LocalDate novaData){
         data = novaData;
      }
 
      @Override
      public String toString() {
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+        return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
      }
 
      
